e509ea57f102dcbd331875d8fe8441f7769289b3
osv2012 <osv2008@live.ru>
2015-12-23 15:32:00 +0300

    MyFix1

---
+++ BlankApp1/AttachedApp1/LoadingProperties.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace AttachedApp1
+{
+    class LoadingProperties : DependencyObject
+    {
+        public static readonly DependencyProperty IsLoadingProperty = DependencyProperty.RegisterAttached("IsLoading", typeof(bool), typeof(LoadingProperties), new PropertyMetadata(true));
+
+        public static void SetIsLoading(UIElement element, Boolean value)
+        {
+            element.SetValue(IsLoadingProperty, value);
+        }
+        public static bool GetIsLoading(UIElement element)
+        {
+            return (bool)element.GetValue(IsLoadingProperty);
+        }
+    }
+}
---
+++ BlankApp1/AttachedApp1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AttachedApp1
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadData();
+        }
+
+        async void LoadData()
+        {
+            this.SetValue(LoadingProperties.IsLoadingProperty, true);
+
+            await Task.Delay(5000);
+
+            this.SetValue(LoadingProperties.IsLoadingProperty, false);
+        }
+    }
+}
